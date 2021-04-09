ac9421f028a94af31f8d1b1ea2e0e8c53a0e12c9
Luke Avedon <lavedon@gmail.com>
2021-04-08 20:52:24 -0400

    multiple api calls running

--- Program.cs
+++ Program.cs
@@ -49,12 +49,11 @@ namespace OxfordV2
 		ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 		var client = new HttpClient();
 		string baseURL = "https://oed-researcher-api.oxfordlanguages.com/oed/api/v0.2/";
-		client.BaseAddress = baseURL;
 
 		Action<object> callWordsAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callWordsAPI");
-			Uri requestURL = new Uri("words/?lemma=mail");
+			Uri requestURL = new Uri(baseURL + "words/?lemma=mail");
 			Console.WriteLine("Making the request");
 			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
@@ -62,41 +61,65 @@ namespace OxfordV2
 		Action<object> callWordsByIdAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callWordsByIdAPI");
+			Uri requestURL = new Uri(baseURL + @"word/orchestra\_nn01?include\_senses=false&include\_quotations=false");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callSensesAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callSensesAPI");
+			Uri requestURL = new Uri(baseURL + @"monitor_nn01/senses/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callQuotationsAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callQuotationsAPI");
+			Uri requestURL = new Uri(baseURL + @"orchestra_nn01/quotations/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callRootsAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callRootsAPI");
+			Uri requestURL = new Uri(baseURL + @"mountainousness_nn01/roots/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callDerivativesAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callDerivativesAPI");
+			Uri requestURL = new Uri(baseURL + @"word/brain_nn01/derivatives/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callSurfaceformsAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callSurfaceformsAPI");
+			Uri requestURL = new Uri(baseURL + @"word/musket_nn02/surfaceforms/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callFrequencyAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callFrequencyAPI");
+			Uri requestURL = new Uri(baseURL + @"word/brain_nn01/frequency/");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 
 		Action<object> callLemmatizeAPI = (Object obj) => 
 		{
 			Console.WriteLine("Called callLemmatizeAPI");
+			Uri requestURL = new Uri(baseURL + @"lemmatize/?form=peas");
+			Console.WriteLine("Making the request");
+			Console.WriteLine(client.GetStringAsync(requestURL));
 		};
 		// @TODO an action to set headers
 		var callAPI = new Task<string>( () => { Console.WriteLine("Task {0} (syncTask) executing on Thread {1}",
@@ -105,14 +128,16 @@ namespace OxfordV2
 						// Reset headers before each call
 						resetHeaders(client);
 						string cat = "muffin";
-						Task apiCall = Task.Factory.StartNew(callWordsAPI, "test");
+						Task apiCall = Task.Factory.StartNew(callLemmatizeAPI, "test");
 						apiCall.Wait();
+						Thread.Sleep(500);
+						Task apiCall2 = Task.Factory.StartNew(callWordsByIdAPI, "2nd test");
+						apiCall2.Wait();
 
 						return cat;
 						});
 		callAPI.RunSynchronously();
 		Console.WriteLine("A crazy cat is named {0}", callAPI.Result);
-		
 	}
     }
 }
