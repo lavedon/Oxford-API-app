899220ca589d9314fc769c794617344f9c7a4b8b
Luke Avedon <lavedon@gmail.com>
2021-04-20 19:26:07 -0400

    sense class added

---
+++ Sense.cs
@@ -0,0 +1,11 @@
+namespace OxfordV2
+{
+	public class Sense
+	{
+		public string Usage { get; set; }
+		public string Definition { get; set; }
+		public bool MainCurrentSense { get; set; }
+		public string SenseGroup {get; set; }
+		public int Start { get; set; } 
+	}
+}
