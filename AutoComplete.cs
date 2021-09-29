b56e989eaafb58b3311853e0e1a75a10e94206a4
Luke <lavedon@gmail.com>
2021-09-28 22:28:49 -0400

    updates

--- AutoComplete.cs
+++ AutoComplete.cs
@@ -18,32 +18,88 @@ namespace oed
         }
         public static StringBuilder HandleTabInput(StringBuilder builder)
         {
-            var currentInput = builder.ToString();
+            var currentInput = builder.ToString()!;
             if (string.IsNullOrWhiteSpace(currentInput))
             {
                 return builder;
             }
             // @TODO: trim the input string to only auto-complete relevant sections
             string match;
-            if (currentInput.Contains("el "))
+            // @TODO change from "Contains" to Read X characters before cursor position - or end of input)
+            string[] inputTokens = currentInput.Split(" ");
+            if (inputTokens.Length > 0)
+            {
+                for (int i = inputTokens.Length - 1; i >= inputTokens.Length - 5; i--)
+                {
+                    if (inputTokens[i] == "el" || inputTokens[i] == "ps" || inputTokens[i] == "et" || inputTokens[i] == "rr" || inputTokens[i] == "ru")
+                    {
+                        break;
+                    }
+                    if (inputTokens[i] == "t")
+                    {
+                        var relevantInput = inputTokens.SkipWhile(t => t != "t");
+                        string renderedInput = string.Join(" ", relevantInput);
+                        return parseCompletion(ref builder, currentInput, out match, token: "t", CompletionData.Topics, offset: 2);
+                    }
+                }
+            }
+
+            switch (inputTokens[^2])
+            {
+                case "el":
+                    return parseCompletion(ref builder, currentInput, out match, token: "el", CompletionData.ELanguageData);
+                case "ps":
+                    return parseCompletion(ref builder, currentInput, out match, token: "ps", CompletionData.PartsOfSpeech);
+                case "et":
+                    return parseCompletion(ref builder, currentInput, out match, token: "et", CompletionData.ETypesData);
+                /*
+                case "t":
+                        var relevantInput = inputTokens.SkipWhile(t => t != "t");
+                        string renderedInput = string.Join(" ", relevantInput);
+                        return parseCompletion(ref builder, currentInput, out match, token: "t", CompletionData.Topics, offset: 2);
+                */
+                case "rr":
+                    return parseCompletion(ref builder, currentInput, out match, token: "rr", CompletionData.Regions);
+                case "ru":
+                    return parseCompletion(ref builder, currentInput, out match, token: "ru", CompletionData.Usage);
+            }
+
+            /*
+            if (currentInput.EndsWith("el "))
             {
                 return parseCompletion(ref builder, currentInput, out match, token: "el", CompletionData.ELanguageData);
             } else if (currentInput.Contains("ps ")) 
             {
                 return parseCompletion(ref builder, currentInput, out match, token: "ps", CompletionData.PartsOfSpeech);
+            } else if (currentInput.Contains("et "))
+            {
+                return parseCompletion(ref builder, currentInput, out match, token: "et", CompletionData.ETypesData);
+                // @TODO: must make sure there is nothing before t 
+            } else if (currentInput.Contains("t "))
+            {
+                return parseCompletion(ref builder, currentInput, out match, token: "t", CompletionData.Topics, offset: 2);
+            } else if (currentInput.Contains("rr "))
+            {
+                return parseCompletion(ref builder, currentInput, out match, token: "rr", CompletionData.Regions);
+            } else if (currentInput.Contains("ru "))
+            {
+                return parseCompletion(ref builder, currentInput, out match, token: "ru", CompletionData.Usage);
             }
+            */
+
             return builder;
 
         } // end contains El
 
         private static StringBuilder parseCompletion(ref StringBuilder builder, string currentInput, out string match, 
-            string token, string[] completionList)
+            string token, string[] completionList, int offset = 3)
         {
-            string relevantInput = currentInput.Substring(currentInput.IndexOf(token) + 3);
+            try {
+            string relevantInput = currentInput.Substring(currentInput.IndexOf(token) + offset);
             if (string.IsNullOrWhiteSpace(relevantInput))
             {
                 match = completionList.FirstOrDefault();
-                builder = displayMatch(builder, match, token);
+                builder = displayMatch(builder, match, token, offset);
                 return builder;
             }
             else if (completionList.Contains(relevantInput.Trim()))
@@ -52,35 +108,42 @@ namespace oed
                 if (string.IsNullOrWhiteSpace(nextMatch))
                 {
                     nextMatch = completionList.FirstOrDefault();
-                    builder = displayMatch(builder, nextMatch, token);
+                    builder = displayMatch(builder, nextMatch, token, offset);
                     match = nextMatch;
                     return builder;
 
                 }
-                builder = displayMatch(builder, nextMatch, token);
+                builder = displayMatch(builder, nextMatch, token, offset);
                 match = nextMatch;
                 return builder;
             }
             else
             {
                 match = completionList.FirstOrDefault(item => item != relevantInput && item.StartsWith(relevantInput, true, CultureInfo.InvariantCulture));
-                builder = displayMatch(builder, match, token);
+                builder = displayMatch(builder, match, token, offset);
                 return builder;
+            }
+            }
+            catch (Exception e)
+            {
+                xConsole.WriteLine("Problem with auto-complete parser.");
+                xConsole.WriteLine(e.Message);
+                match = "";
+                return null;
             }
         }
 
-        public static StringBuilder displayMatch(StringBuilder builder, string match, string token)
+        public static StringBuilder displayMatch(StringBuilder builder, string match, string token, int offset)
         {
             // TODO fix backspace
             // @TODO fix next match
             ClearCurrentLine();
             token = token + " ";
-            string cleanedInput = builder.ToString().Substring(0, builder.ToString().IndexOf(token) + 3);
+            string cleanedInput = builder.ToString().Substring(0, builder.ToString().IndexOf(token) + offset);
             builder.Clear();
             Console.Write(cleanedInput + match);
             builder.Append(cleanedInput + match);
             return builder;
-
         }
 
         public static StringBuilder HandleKeyInput(StringBuilder builder, ConsoleKeyInfo input)
